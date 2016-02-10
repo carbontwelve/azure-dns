33d75ec4e856aeed591ceb8c71515c833a4854d6
Simon Dann <simon.dann@gmail.com>
2016-02-10 18:42:56 +0000

    Using the Proton nano framework

--- src/Events/ResponseSentEvent.php
+++
@@ -1,9 +0,0 @@
-<?php namespace Carbontwelve\AzureDns\Events;
-
-class ResponseSentEvent extends BaseResponseEvent
-{
-    public function getName()
-    {
-        return 'ResponseSentEvent';
-    }
-}

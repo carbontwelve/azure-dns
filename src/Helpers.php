32d59db22c3cfcaffd0ca56037fc2e592aed3b06
Simon Dann <simon@photogabble.co.uk>
2016-02-12 12:30:46 +0000

    Removing old fluff

--- src/Helpers.php
+++ src/Helpers.php
@@ -4,35 +4,6 @@
  * Global helper functions, this is a side effect of using Laravel for so long ;)
  */
 
-if (! function_exists('session')) {
-    function session($key = null, $value = null)
-    {
-        $session = app('session');
-
-        if ( is_null($key) && is_null($value)) {
-            return $session;
-        }
-
-        if ( !is_null($key) && is_null($value)) {
-            return $session;
-        }
-    }
-}
-
-if (!function_exists('asset')) {
-    /**
-     * Generate an asset path for the application.
-     *
-     * @param  string $path
-     * @param  bool $secure
-     * @return string
-     */
-    function asset($path, $secure = null)
-    {
-        return app('url')->asset($path, $secure);
-    }
-}
-
 if (!function_exists('dd')) {
     /**
      * Dump and die

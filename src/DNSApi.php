176c1b62cdfdcb502a231ccfb7c98f0aab3ba640
Simon Dann <simon@photogabble.co.uk>
2016-02-16 10:33:17 +0000

    Adding some documentation

--- src/DNSApi.php
+++ src/DNSApi.php
@@ -4,9 +4,13 @@ use AzureDns\Exceptions\IncompleteConfigurationException;
 use TheNetworg\OAuth2\Client\Provider\Azure;
 use Aura\Session\Segment;
 
+/**
+ * Class DNSApi
+ * @package AzureDns
+ * @todo This should cache GET responses, then invalidate them based upon PUT/PATCH actions (cache invalidation is hard)
+ */
 class DNSApi
 {
-
     /**
      * @var Segment
      */
@@ -75,6 +79,8 @@ class DNSApi
 
     /**
      * Get the list of subscriptions the user is allowed to see
+     *
+     * @link https://msdn.microsoft.com/en-us/library/azure/dn776325.aspx#ListSubscriptions
      * @return array
      */
     public function getSubscriptionsList()
@@ -93,6 +99,8 @@ class DNSApi
 
     /**
      * Get the list of groups that belong to the configured subscription
+     *
+     * @link https://msdn.microsoft.com/en-us/library/azure/dn790529.aspx
      * @return array
      * @throws \Exception
      */
@@ -110,6 +118,8 @@ class DNSApi
 
     /**
      * Get a list of DNS zones that are attached to the configured resource group
+     *
+     * @link https://msdn.microsoft.com/en-us/library/azure/mt130594.aspx
      * @return array
      * @throws \Exception
      */
@@ -119,6 +129,13 @@ class DNSApi
             [], '2015-05-04-preview');
     }
 
+    /**
+     * Create a new zone
+     *
+     * @link https://msdn.microsoft.com/en-us/library/azure/mt130622.aspx
+     * @param string $zone
+     * @return null
+     */
     public function createZone($zone)
     {
         return $this->putToAPI(
@@ -135,9 +152,16 @@ class DNSApi
         );
     }
 
+    /**
+     * Delete a given zone, if successful the azure api will respond with status code 200.
+     *
+     * @link https://msdn.microsoft.com/en-us/library/azure/mt130587.aspx
+     * @param $zone
+     * @return bool
+     * @throws \Exception
+     */
     public function deleteZone($zone)
     {
-
         $this->azure->API_VERSION = '2015-05-04-preview';
 
         $path = $this->buildAPIPath(
@@ -153,6 +177,8 @@ class DNSApi
 
     /**
      * Get a list of record sets that are attached to the named $zone
+     *
+     * @link https://msdn.microsoft.com/en-us/library/azure/mt130638.aspx
      * @param string $zone
      * @return array
      * @throws \Exception
@@ -192,7 +218,7 @@ class DNSApi
     }
 
     /**
-     * @param $path
+     * @param string $path
      * @param array $attr
      * @param null $version
      * @return array
@@ -212,10 +238,10 @@ class DNSApi
     }
 
     /**
-     * @param $path
+     * @param string $path
      * @param array $attr
-     * @param $body
-     * @param null $version
+     * @param string $body
+     * @param null|string $version
      * @return null
      * @throws \Exception
      */
@@ -235,7 +261,7 @@ class DNSApi
     /**
      * Build the path from an input template string and attributes
      *
-     * @param $path
+     * @param string $path
      * @param array $attr
      * @return mixed
      */
@@ -258,6 +284,8 @@ class DNSApi
     }
 
     /**
+     * If the API returns an error message object then we should throw that error
+     *
      * @param $data
      * @throws \Exception
      */
